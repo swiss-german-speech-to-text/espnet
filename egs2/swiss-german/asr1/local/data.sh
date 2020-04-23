b73f7aca9880454f4d6b5a58d1dd9a9878e712c8
Lukas Neukom <lukas.neukom@gmail.com>
2020-04-23 15:46:05 +0200

    fix download

--- egs2/swiss-german/asr1/local/data.sh
+++ egs2/swiss-german/asr1/local/data.sh
@@ -35,9 +35,9 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     mkdir -p ${raw_data_de}
     mkdir -p ${raw_data_ch_train}
     mkdir -p ${raw_data_ch_test}
-    #local/download_and_untar.sh ${raw_data_de} https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-3/de.tar.gz de.tar.gz
-    wget https://drive.switch.ch/index.php/s/PpUArRmN5Ba5C8J/download?path=%2F&files=train.zip -P ${raw_data_ch_train}
-    wget https://drive.switch.ch/index.php/s/PpUArRmN5Ba5C8J/download?path=%2F&files=test.zip -P ${raw_data_ch_test}
+    local/download_and_untar.sh ${raw_data_de} https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com/cv-corpus-3/de.tar.gz de.tar.gz
+    wget https://drive.switch.ch/index.php/s/PpUArRmN5Ba5C8J/download?path=%2F&files=train.zip -O ${raw_data_ch_train}/train.zip
+    wget https://drive.switch.ch/index.php/s/PpUArRmN5Ba5C8J/download?path=%2F&files=test.zip -O ${raw_data_ch_test}/test.zip
     unzip ${raw_data_ch_train}/train.zip
     unzip ${raw_data_ch_test}/test.zip
 fi
@@ -48,7 +48,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     ### But you can utilize Kaldi recipes in most cases
     local/data_prep.pl ${raw_data_de} "validated" data/de_train
     local/data_prep.pl ${raw_data_ch_train} "data" data/ch_train_dev
-    #local/data_prep.pl ${raw_data_ch_test} "data" data/ch_test
+    #local/data_prep.pl ${raw_data_ch_test} "data" data/ch_test FIXME transform text data so we can use it
 
     # split ch train data into train and dev
     ./utils/subset_data_dir_tr_cv.sh data/ch_train_dev data/ch_train data/ch_dev
