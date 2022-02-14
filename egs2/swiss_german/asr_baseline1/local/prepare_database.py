135648c8621c11c2ee69af275c886e39ebc6089b
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-02-14 22:06:06 +0100

    Update preprocessing

--- egs2/swiss_german/asr_baseline1/local/prepare_database.py
+++ egs2/swiss_german/asr_baseline1/local/prepare_database.py
@@ -1,3 +1,4 @@
+import os.path
 import sys
 
 import pandas as pd
@@ -5,7 +6,11 @@ from prepare_text import normalize_text, SG_CHAR_VOCAB, SG_CHAR_LOOKUP
 
 
 def main(source_csv: str, target: str):
+    assert os.path.exists(source_csv), f"{source_csv} does not exist"
+    assert os.path.isfile(source_csv), f"{source_csv} is not a file"
     df = pd.read_csv(source_csv, sep='\t', encoding='utf-8')
+    assert (df.keys() == ['client_id', 'path', 'sentence', 'up_votes', 'down_votes', 'age',
+       'gender', 'accent', 'iou_estimate']).all(), "Not all columns present"
     df.sentence = df.sentence.apply(lambda text: normalize_text(text, SG_CHAR_VOCAB, SG_CHAR_LOOKUP))
     df.to_csv(target, sep="\t", encoding='utf-8')
 
