7544cd6bdf3a0dc1bed1509a403223a1879c2044
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-02-15 09:58:56 +0100

    Fix whitespace removal

--- egs2/swiss_german/asr_baseline1/local/prepare_text.py
+++ egs2/swiss_german/asr_baseline1/local/prepare_text.py
@@ -1,7 +1,7 @@
 import re
 from typing import Dict, Set
 
-WHITESPACE_REGEX = re.compile(r'[ \t]+')
+WHITESPACE_REGEX = re.compile(r'\s+')
 
 SG_CHAR_VOCAB = {
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
