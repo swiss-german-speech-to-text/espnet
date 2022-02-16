e994cb2fb79ffb6dd6786351a1cc4434bf3c92ec
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-02-16 16:48:19 +0100

    Increase memory usage & fix bpe training

--- egs2/swiss_german/asr_baseline1/run.sh
+++ egs2/swiss_german/asr_baseline1/run.sh
@@ -32,5 +32,6 @@ nbpe=5000
     --valid_set "${train_dev}" \
     --test_sets "${test_set}" \
     --bpe_train_text "data/${lm_train_set}" \
+    --bpe_input_sentence_size=1000000 \
     --lm_train_text "data/${lm_train_set}" \
     --lm_dev_text "data/${lm_val_set}" "$@"
