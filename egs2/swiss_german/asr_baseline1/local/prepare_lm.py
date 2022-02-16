6a699baa4ea4d105111e5bb4a3c728bdd697d02d
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-02-16 20:32:29 +0100

    Add unique id to lm set

--- egs2/swiss_german/asr_baseline1/local/prepare_lm.py
+++ egs2/swiss_german/asr_baseline1/local/prepare_lm.py
@@ -6,10 +6,13 @@ from prepare_text import normalize_text, SG_CHAR_VOCAB, SG_CHAR_LOOKUP
 
 def main(source_text: str, target_dir: str):
     os.mkdir(target_dir)
+    counter = 0
     with open(source_text, 'r', encoding='utf-8') as input:
         with open(f'{target_dir}/text', mode='w', encoding='utf-8') as output:
             for sentence in input:
                 text = normalize_text(sentence, SG_CHAR_VOCAB, SG_CHAR_LOOKUP)
+                text = str(counter) + " " + text
+                counter += 1
                 if len(text.rstrip().split(maxsplit=1)) == 2:
                     output.write(f"{text}\n")
 
