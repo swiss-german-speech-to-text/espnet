6148d7db602274ff61152501c98267b93da513cc
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-02-17 11:44:45 +0100

    Set upper bound for sentence length

--- egs2/swiss_german/asr_baseline1/local/prepare_lm.py
+++ egs2/swiss_german/asr_baseline1/local/prepare_lm.py
@@ -10,12 +10,12 @@ def main(source_text: str, target_dir: str):
     with open(source_text, 'r', encoding='utf-8') as input:
         with open(f'{target_dir}/text', mode='w', encoding='utf-8') as output:
             for line in input:
-                for sentence in sent_tokenize(line):
+                for sentence in sent_tokenize(line, language='german'):
                     text = normalize_text(sentence, SG_CHAR_VOCAB, SG_CHAR_LOOKUP)
                     text = str(counter) + " " + text
-                    counter += 1
-                    if len(text.rstrip().split(maxsplit=1)) == 2:
+                    if len(text.rstrip().split(maxsplit=1)) == 2 and len(text) <= 2000:
                         output.write(f"{text}\n")
+                        counter += 1
 
 
 if __name__ == '__main__':
