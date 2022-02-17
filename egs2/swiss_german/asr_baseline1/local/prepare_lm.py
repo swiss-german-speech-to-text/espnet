3e16c87bf3da55c1b788f233fe422e3df6e80e43
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-02-17 09:32:29 +0100

    Sentences are way too long, try nltk sentence split

--- egs2/swiss_german/asr_baseline1/local/prepare_lm.py
+++ egs2/swiss_german/asr_baseline1/local/prepare_lm.py
@@ -2,19 +2,20 @@ import os
 import sys
 
 from prepare_text import normalize_text, SG_CHAR_VOCAB, SG_CHAR_LOOKUP
-
+from nltk.tokenize import sent_tokenize
 
 def main(source_text: str, target_dir: str):
     os.mkdir(target_dir)
     counter = 0
     with open(source_text, 'r', encoding='utf-8') as input:
         with open(f'{target_dir}/text', mode='w', encoding='utf-8') as output:
-            for sentence in input:
-                text = normalize_text(sentence, SG_CHAR_VOCAB, SG_CHAR_LOOKUP)
-                text = str(counter) + " " + text
-                counter += 1
-                if len(text.rstrip().split(maxsplit=1)) == 2:
-                    output.write(f"{text}\n")
+            for line in input:
+                for sentence in sent_tokenize(line):
+                    text = normalize_text(sentence, SG_CHAR_VOCAB, SG_CHAR_LOOKUP)
+                    text = str(counter) + " " + text
+                    counter += 1
+                    if len(text.rstrip().split(maxsplit=1)) == 2:
+                        output.write(f"{text}\n")
 
 
 if __name__ == '__main__':
