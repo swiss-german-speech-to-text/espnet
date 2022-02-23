3d9c1de52ad225ece3a644c9d6af2b1dc09b5197
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-02-23 14:08:48 +0100

    Try using espnet slurm backend

--- egs2/TEMPLATE/asr1/asr.sh
+++ egs2/TEMPLATE/asr1/asr.sh
@@ -22,7 +22,8 @@ min() {
 }
 SECONDS=0
 
-SLURM_SETTINGS="ml purge & ml CMake/3.15.3-GCCcore-8.3.0 & ml SoX/14.4.2-GCCcore-8.3.0 & ml libsndfile/1.0.28-GCCcore-8.3.0 & ml FFmpeg/.4.2.1-GCCcore-8.3.0 & ml FLAC/1.3.3-GCCcore-8.3.0 & ml cuDNN/8.1.0.77-CUDA-11.2.1 & ml Python/3.7.4-GCC core-8.3.0 & source /scicore/home/graber0001/schran0000/espnet/tools/venv/bin/activate & export PATH=/scicore/home/graber0001/schran0000/opt/bin:$PATH & export LD_LIBRARY_PATH=/scicore/home/graber0001/schran0000/opt/lib:$LD_LIBRARY_PATH & export PKG_CONFIG_PATH=/scicore/home/graber0001/schran0000/opt/lib/pkgconfig:$PKG_CONFIG_PATH & export WANDB_DIR=/scicore/home/graber0001/schran0000/wandb & export TMPDIR=/scicore/home/graber0001/schran0000/wandb"
+SLURM_SETTINGS="ml purge & ml CMake/3.15.3-GCCcore-8.3.0 & ml SoX/14.4.2-GCCcore-8.3.0 & ml libsndfile/1.0.28-GCCcore-8.3.0 & ml FFmpeg/.4.2.1-GCCcore-8.3.0 & ml FLAC/1.3.3-GCCcore-8.3.0 & ml cuDNN/8.1.0.77-CUDA-11.2.1 & ml Python/3.7.4-GCC core-8.3.0"
+
 
 # General configuration
 stage=1              # Processes starts from the specified stage.
