0bae61e72da66ef8deb6a69b4dc9e35af200184b
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-03-10 16:02:06 +0100

    Update snf data

--- egs2/swiss_german/asr_baseline2/run.sh
+++ egs2/swiss_german/asr_baseline2/run.sh
@@ -17,24 +17,10 @@ inference_config=conf/decode_asr.yaml
 
 nbpe=5000
 
-ml CMake/3.15.3-GCCcore-8.3.0
-ml SoX/14.4.2-GCCcore-8.3.0
-ml libsndfile/1.0.28-GCCcore-8.3.0
-ml FFmpeg/.4.2.1-GCCcore-8.3.0
-ml FLAC/1.3.3-GCCcore-8.3.0
-ml cuDNN/8.1.0.77-CUDA-11.2.1
-ml Python/3.7.4-GCCcore-8.3.0
-
-source /scicore/home/graber0001/schran0000/espnet/tools/venv/bin/activate
-export PATH=/scicore/home/graber0001/schran0000/opt/bin:$PATH
-export LD_LIBRARY_PATH=/scicore/home/graber0001/schran0000/opt/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=/scicore/home/graber0001/schran0000/opt/lib/pkgconfig:$PKG_CONFIG_PATH
-export WANDB_DIR=/scicore/home/graber0001/schran0000/wandb
-export TMPDIR=/scicore/home/graber0001/schran0000/wandb
-
 ./asr.sh \
     --stage 0 \
     --stop_stage 1000 \
+    --asr_exp "exp2" \
     --ngpu 4 \
     --use_lm false \
     --lang de \
