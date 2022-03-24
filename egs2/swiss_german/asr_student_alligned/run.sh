544b12c275f03f1ece108b02b4b3585ec11e9a9b
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-03-24 16:10:21 +0100

    Fix paths

--- egs2/swiss_german/asr_student_alligned/run.sh
+++ egs2/swiss_german/asr_student_alligned/run.sh
@@ -28,28 +28,28 @@ else
 fi
 
 # Train teacher
-#local/asr.sh \
-#    --stage 0 \
-#    --stop_stage 4 \
-#    --ngpu 4 \
-#    --lang "${lang}" \
-#    --dumpdir $TMPDIR/dump \
-#    --expdir $TMPDIR/exp \
-#    --use_lm true \
-#    --lm_config "${lm_config}" \
-#    --token_type bpe \
-#    --nbpe $nbpe \
-#    --feats_type raw \
-#    --speed_perturb_factors "0.9 1.0 1.1" \
-#    --asr_config "${asr_config}" \
-#    --inference_config "${inference_config}" \
-#    --train_set "${train_set}" \
-#    --valid_set "${train_dev}" \
-#    --test_sets "${test_set}" \
-#    --bpe_train_text "data/${lm_train_set}" \
-#    --bpe_input_sentence_size 10000000 \
-#    --lm_dev_text "data/${lm_val_set}" \
-#    --lm_train_text "data/${lm_val_set}" "$@"
+local/asr.sh \
+    --stage 0 \
+    --stop_stage 4 \
+    --ngpu 4 \
+    --lang "${lang}" \
+    --dumpdir $TMPDIR/dump \
+    --expdir $TMPDIR/exp \
+    --use_lm true \
+    --lm_config "${lm_config}" \
+    --token_type bpe \
+    --nbpe $nbpe \
+    --feats_type raw \
+    --speed_perturb_factors "0.9 1.0 1.1" \
+    --asr_config "${asr_config}" \
+    --inference_config "${inference_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${train_dev}" \
+    --test_sets "${test_set}" \
+    --bpe_train_text "data/${lm_train_set}" \
+    --bpe_input_sentence_size 10000000 \
+    --lm_dev_text "data/${lm_val_set}" \
+    --lm_train_text "data/${lm_val_set}" "$@"
 
 
 # Decode data
