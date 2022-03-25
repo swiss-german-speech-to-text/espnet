9131fff8ccb1698d933fb5edbeb2d1282b5f40a1
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-03-25 12:31:13 +0100

    Fix paths

--- egs2/swiss_german/asr_student_unalligned/local/prepare_database.py
+++ egs2/swiss_german/asr_student_unalligned/local/prepare_database.py
@@ -8,7 +8,7 @@ from prepare_text import normalize_text, SG_CHAR_VOCAB, SG_CHAR_LOOKUP
 def main(source_csv: str):
     columns = ['client_id', 'path', 'sentence', 'up_votes', 'down_votes', 'age',
                'gender', 'accent', 'iou_estimate']
-    path = [os.path.dirname(source_csv) + '/clips/' + file for file in list(filter(lambda x: x.endswith('.flac'), os.listdir(os.path.dirname(source_csv) + '/clips')))]
+    path = list(filter(lambda x: x.endswith('.flac'), os.listdir(os.path.dirname(source_csv) + '/clips')))
     client_id = list(range(0,len(path)))
     sentence = ["Dummy"] * len(path)
     up_votes = [""] * len(path)
