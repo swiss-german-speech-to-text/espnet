2d0276e317be33d316a58e8a64e483f4be36d77e
Yanick Schraner <yanick.schraner@fhnw.ch>
2022-03-25 08:40:06 +0100

    Fix paths

--- egs2/swiss_german/asr_student_unalligned/local/prepare_database.py
+++ egs2/swiss_german/asr_student_unalligned/local/prepare_database.py
@@ -6,8 +6,6 @@ from prepare_text import normalize_text, SG_CHAR_VOCAB, SG_CHAR_LOOKUP
 
 
 def main(source_csv: str):
-    assert os.path.exists(source_csv), f"{source_csv} does not exist"
-    assert os.path.isfile(source_csv), f"{source_csv} is not a file"
     columns = ['client_id', 'path', 'sentence', 'up_votes', 'down_votes', 'age',
                'gender', 'accent', 'iou_estimate']
     path = [os.path.dirname(source_csv) + '/clips/' + file for file in list(filter(lambda x: x.endswith('.flac'), os.listdir(os.path.dirname(source_csv) + '/clips')))]
